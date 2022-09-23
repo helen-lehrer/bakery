7db6a1862af9cae463751cd2123313ca75a87067
Helen Lehrer <hlehrer@outlook.com>
2022-09-23 09:11:39 -0700

    Pass 1st test for BreadTests

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,7 +4,14 @@ namespace Bakery.Models
   {
     public int BreadCost(int numLoaves)
     {
+        if (numLoaves == 1)
+        {
+            return 5;
+        }
+        else
+        {
         return 1;
+        }
     }
     // properties, methods, etc. will go here.
 
